6f56006a70db977a67dc8e5cab6d32285f8af4c4
sm7515 <sm7515@nyu.edu>
2021-04-14 22:00:08 -0400

    done with css

--- src/App.js
+++ src/App.js
@@ -57,20 +57,19 @@ class App extends React.Component {
                 <h3 className='card-title invitation-title'>
                   曼哈顿档案：夜幕降临
                 </h3>
-                <p className='card-title invitation-subtitle'>
+                <h5 className='card-title invitation-subtitle'>
                   体验以纽约为背景的剧本杀~
-                </p>
+                </h5>
               </div>
               <div className='invitation-text'>
                 <p className='card-text'>
                   NYUCSSA诚意推出全新线上剧本杀活动
-                  3.23～3.25【曼哈顿档案：夜幕降临】。本次活动共有三个剧本，每个剧本由6~7个角色组成。我们将根据您本次的测试结果，提供相似的剧本角色供您报名时进行参考。通过本次活动，您将结识更多有趣的小伙伴，并与他们一起斗智斗勇，合作办案。本次活动人数有限，赶快抓紧时间报名吧！让我们在这个春天，和同伴们一起探索纽约的真相吧~
+                  3.23～3.25 【曼哈顿档案：夜幕降临】。本次活动共有三个剧本，每个剧本由6~7个角色组成。我们将根据您本次的测试结果，提供相似的剧本角色供您报名时进行参考。通过本次活动，您将结识更多有趣的小伙伴，并与他们一起斗智斗勇，合作办案。本次活动人数有限，赶快抓紧时间报名吧！让我们在这个春天，和同伴们一起探索纽约的真相吧~
                 </p>
                 <p className='note'>
-                  注：报名本次活动请扫描 结果页下方 的二维码
+                  注：报名本次活动请扫描结果页下方的二维码
                 </p>
               </div>
-              <div className='answers'>
                 <button
                   type='button'
                   className='btn btn-primary inv-btn'
@@ -80,7 +79,6 @@ class App extends React.Component {
                 >
                   查看结果
                 </button>
-              </div>
             </div>
           </div>
         </div>
@@ -91,8 +89,10 @@ class App extends React.Component {
   render_start() {
     return (
       <div className='container test-start'>
-        <h2>测测你是纽约的</h2>
-        <h1>哪种食物</h1>
+        <div>
+          <h3>测测你是</h3>
+          <h2>纽约的哪种食物</h2>
+        </div>
         <button
           type='button'
           className='btn start-btn'
@@ -117,26 +117,63 @@ class App extends React.Component {
 
   generate_image() {
     if (document.getElementById('canvasImg') != null) return;
-    const element = document.getElementById('test-result');
-    html2canvas(element, { height: 900 }).then(function (canvas) {
+    const element = document.getElementById('result-container');
+    html2canvas(element).then(function (canvas) {
       let base64Url = canvas.toDataURL();
       const imgDiv = document.createElement('div');
       imgDiv.className = 'img-container';
 
       const closeBtn = document.createElement('button');
       closeBtn.textContent = 'close';
-      closeBtn.className = 'close-btn';
+      closeBtn.className = 'btn close-btn';
       imgDiv.appendChild(closeBtn);
 
       const img = document.createElement('img');
       img.id = 'canvasImg';
       img.src = base64Url;
-      img.style.width = '400px';
       imgDiv.appendChild(img);
 
       document.body.appendChild(imgDiv);
       closeBtn.addEventListener('click', () => imgDiv.remove());
     });
+  }
+
+  render_roles(){
+        const result = this.generate_result();
+
+    return(
+            <div className='result-container' id='result-container'>
+               <div className='container roles'>
+          <h3>你最适合的角色：</h3>
+          <div className='roles-container'>
+          {plays.map((name, i) => (
+            <div key={i}>
+              <h2>{`剧本${i + 1}：${name}`}</h2>
+              <h5>{result.roles[i * 2]}</h5>
+              <h5>{result.roles[i * 2 + 1]}</h5>
+            </div>
+          ))}</div>
+        </div>
+
+<div className='btn-class'>
+          <button onClick={() => this.generate_image()} className='btn generate_img'>生成图片</button>
+          <button onClick={() => this.setState({state:'finished'})} className='btn role_result'>查看结果</button>
+        </div>
+
+        <div className='qrcode'>
+            <div>
+                            <p>扫二码查看剧本简介</p>
+
+              <img src="http://192.168.1.205:3000/wx-qrcode.png" alt='qrcode'></img>
+            </div>
+            <div>
+                            <p>扫二维码立即报名活动</p>
+
+                 <img src="http://192.168.1.205:3000/wx-qrcode.png" alt='qrcode'></img>
+            </div>
+          </div>
+</div>
+    )
   }
 
   render_finished() {
@@ -145,7 +182,7 @@ class App extends React.Component {
       (prev, cur) => (prev = prev + ', ' + cur),
     );
     return (
-      <div className='container'>
+      <div className='result-container' id='result-container'>
         <div className='container test-result' id='test-result'>
           <h3>与您最符合的食物是：</h3>
           <h1 className='mytitle'>{result.food}</h1>
@@ -153,44 +190,41 @@ class App extends React.Component {
           <h2>{places}</h2>
           <div className='desc'>{result.desc}</div>
           <br />
-          <div className='qrcode'>
+         
+        </div>
+        <div className='btn-class'>
+          <button onClick={() => this.generate_image()} className='btn generate_img'>生成图片</button>
+          <button onClick={() => this.setState({state:'roles'})} className='btn role_result'>查看角色</button>
+        </div>
+         <div className='qrcode'>
             <div>
-              <p>扫二维码查看剧本简介</p>
+                            <p>扫二码查看剧本简介</p>
+
+              <img src="http://192.168.1.205:3000/wx-qrcode.png" alt='qrcode'></img>
             </div>
             <div>
-              <p>扫二维码立即报名活动</p>
+                            <p>扫二维码立即报名活动</p>
+
+                 <img src="http://192.168.1.205:3000/wx-qrcode.png" alt='qrcode'></img>
             </div>
           </div>
-        </div>
-        <button onClick={() => this.generate_image()}>生成图片</button>
-        <div className='container roles'>
-          <h3>你最适合的角色：</h3>
-          {plays.map((name, i) => (
-            <div key={i}>
-              <h2>{`剧本${i + 1}：${name}`}</h2>
-              <h5>{result.roles[i * 2]}</h5>
-              <h5>{result.roles[i * 2 + 1]}</h5>
-            </div>
-          ))}
-        </div>
       </div>
     );
   }
 
   render_question() {
     return (
-      <div className='container'>
-        <div className='card question'>
-          <div className='card-body'>
+      <div className='question-container'>
+        <div className='card-question'>
+            <h4>Question {this.state.current_question}</h4>
             <h5 className='card-title'>
               {questions[this.state.current_question][0]}
             </h5>
-            <div className='answers'>
-              {questions[this.state.current_question][1].map(
-                this.render_ans.bind(this),
-              )}
-            </div>
-          </div>
+        </div>
+        <div className='answers'>
+          {questions[this.state.current_question][1].map(
+            this.render_ans.bind(this),
+          )}
         </div>
       </div>
     );
@@ -201,6 +235,8 @@ class App extends React.Component {
       return this.render_start();
     } else if (this.state.state === 'finished') {
       return this.render_finished();
+    } else if(this.state.state==='roles'){
+      return this.render_roles();
     } else if (this.state.state === 'testing') {
       return this.render_question();
     } else if (this.state.state === 'invitation') {
